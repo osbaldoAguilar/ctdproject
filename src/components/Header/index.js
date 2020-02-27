88295bc8d6f3e23efe8454fa01b9ea8a03e64c4c
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-02-27 00:45:44 -0500

    clean up so code and change background color

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import '../../styles/header.css'
 
-const Header = (props) => {
-  const projectList = props.projectData.map((project) => {
+const Header = (props) => {             // header of page
+  const projectList = props.projectData.map((project) => {    
+    const projectTitles = project.fields.Name.toUpperCase();
     return(
-      <div key = {project.id} 
-      onClick = {() => props.selectProject(project.id)}
-      className = 'card'
-      style = {{
-        fontSize: "15px",
-        fontColor: "black"
-      }}>
-        <p>{project.fields.Name}</p>
+      <div className = 'backgrounds'>
+        <div key = {project.id} 
+        onClick = {() => props.selectProject(project.id)}
+        className = 'card'
+        style = {{
+          fontSize: "15px",
+          fontColor: "black"
+        }}>
+          <p>{projectTitles}</p>
+        </div>
       </div>
     )
   });
+  
   console.log('this ', projectList)
   // const projectList = props.projectData.map((project) => project
   // console.log('this ',projectList)
