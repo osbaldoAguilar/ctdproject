2f6a24bbd4ef9b607333fd34a342ac5a781e8880
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-02-27 21:47:42 -0500

    full of errors fix header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { render } from 'react-dom';      
-import shuffle from 'lodash/shuffle';     
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+// import { render } from 'react-dom';      // may not need 
+// import shuffle from 'lodash/shuffle';      // may not need
 // import data from './data';              // may not need
 import '../../styles/header.css';
 
-const Header = (props) => {             // header of page
+const Header = (props) => { 
+  
+  onSlideChange(e) ({
+    console.log('position during a change: ', e.item);
+    console.log('Slide\'s position during a change: ', e.slide);
+  })
+
+  onSlideChanged(e) {
+    console.log('position during a change: ', e.item);
+    console.log('Slide\'s position during a change: ', e.slide); 
+  }
+  renderThumbs = () =>
+  <ul>
+    {
+      [1, 2, 3, 4, 5].map((item, i) =>
+        <li key = {i} onClick = {() => this.Carousel._onDotClick(i) }> Thumb {item}></li>)
+    }
+  </ul>
+             // header of page
   const projectList = props.projectData.map((project) => {    
     const projectTitles = project.fields.Name.toUpperCase();
     return(
@@ -20,12 +39,56 @@ const Header = (props) => {             // header of page
   
   console.log('this ', projectList)
   return(
-    <ul className = 'container project-list'>
+    <AliceCarousel className = 'container project-list'>
       {projectList}
-    </ul>
+    </AliceCarousel>
   )
 }
 
+class Gallery extends React.Component {
 
+  onSlideChange(e) {
+    console.log('position during a change: ', e.item);
+    console.log('Slide\'s position during a change: ', e.slide);
+  }
+
+  onSlideChanged(e) {
+    console.log('position during a change: ', e.item);
+    console.log('Slide\'s position during a change: ', e.slide); 
+  }
+
+  render() {
+    const responsive = {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1024: {
+        items: 3
+      }
+    };
+    
+    return(
+      <AliceCarousel
+        duration = {400}
+        autoPlay = {true}
+        startIndex = {1}
+        fadeOutAnimation = {true}
+        mouseDragEnabled = {true}
+        playButtonEnabled = {true}
+        responsive = {responsive}
+        autoPlayInterval = {2000}
+        autoPlayDirection = 'rtl'
+        autoPlayActionDisabled = {true}
+        onSlideChange = {this.onSlideChange}
+        onSlideChanged = {this.onSlideChanged}
+        >
+          <h4>{projectList}</h4>
+        </AliceCarousel>
+    )
+  }
+}
 
 export default Header
