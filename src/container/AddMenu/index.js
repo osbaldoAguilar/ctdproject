8dd7f72bcfd05471275e2c54bf2b4f7e5aaa906d
osbaldo aguilar <osbaldoaguilar@osbaldos-MacBook-Air.local>
2020-03-06 15:56:50 -0500

    AddingWorxAxios

--- src/container/AddMenu/index.js
+++ src/container/AddMenu/index.js
@@ -1,106 +1,38 @@
-// import React from "react";
-
-// class AddMenu extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { name: "", status: "" };
-
-//     this.handleName = this.handleName.bind(this);
-//     this.handleStatus = this.handleStatus.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleName(event) {
-//     const value = event.target.value;
-//     this.setState({ [event.target.value]: value });
-//   }
-
-//   handleStatus(event) {
-//     this.setState({ status: event.target.vaule });
-//   }
-
-//   handleSubmit(event) {
-//     console.log("A name was submitted: " + this.state.name);
-//     console.log("Status:", this.state.status);
-//     event.preventDefault();
-//   }
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           Name:
-//           <input
-//             type="text"
-//             value={this.state.name}
-//             onChange={this.handleName}
-//           />
-//         </label>
-//         <label>
-//           Status:
-//           <input
-//             type="text"
-//             value={this.state.status}
-//             onChange={this.handleName}
-//           />
-//         </label>
-//         <input type="submit" value="Submit" />
-//       </form>
-//     );
-//   }
-// }
-
-// export default AddMenu;
-
 import React from "react";
-// import Form from "../../components/Form/index";
+import axios from "axios";
 
 export default class AddMenu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "",
-      feilds: {
+      // value: "",
+      fields: {
         Name: ""
       }
-
-      //   userData: {}
     };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
-  //   componentDidMount() {
-  //     this.push2AirTable();
-  //   }
 
-  push2AirTable() {
-    let userData = {
+  push2AirTable = async e => {
+    e.preventDefault();
+    const { Name } = this.state.fields;
+
+    let fields = {
       fields: {
-        Name: this.state.Name
-        // Status: this.state.status,
-        // Current_Team: this.state.team
+        Name
       }
+      // Status: this.state.status,
+      // Current_Team: this.state.team
     };
 
-    const url = "https://api.airtable.com/v0/appQSPi3XUdUMbM1m/Projects";
+    const url = `https://api.airtable.com/v0/appQSPi3XUdUMbM1m/Projects?api_key=${process.env.REACT_APP_AIRTABLE_KEY}`;
 
-    fetch(url, {
-      headers: {
-        Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY
-        // Accept: "application/json"
-      },
-      method: "POST",
-      body: userData
-    })
-      .then(response => response.json())
-      .then(responseData => {
-        console.log("push2: ", responseData);
-        // const formData = responseData.records;
-        // console.log("formData ", formData);
-        // this.setState({ formData: formData });
-      })
-      .catch(error => console.log(error));
-  }
+    try {
+      const response = await axios.post(url, fields);
+      console.log("airtables response \n", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   //   handleChange = event => {
   //     setValue(event.target.value);
   //     // this.setState({ Name: this.state.Name });
@@ -115,21 +47,28 @@ export default class AddMenu extends React.Component {
   //   };
 
   render() {
+    let userInput = {
+      fields: {
+        Name: this.state.Name
+      }
+    };
     return (
       <div className="view-container-menu">
-        <div className="menu-content">
+        <form className="menu-content" onSubmit={this.push2AirTable}>
           <label style={{ color: "#475c67" }}>
             Project Name:
             <input
               type="text"
               //   value={this.state.Name}
-              onChange={text => this.setState({ Name: text })}
+              onChange={event =>
+                this.setState({ fields: { Name: event.target.value } })
+              }
             />
-            <button type="submit" value="Submit" onClick={this.push2AirTable}>
+            <button type="submit" value="Submit">
               submit
             </button>
           </label>
-        </div>
+        </form>
       </div>
     );
   }
--- src/container/DashBoard/index.js
+++ src/container/DashBoard/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "../../components/styles/images/logo.png";
 import { Block, Header } from "../../components";
+import axios from "axios";
 
 class DashBoard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       projectData: []
-      // selectedProject: {}
     };
   }
   componentDidMount() {
@@ -24,25 +24,22 @@ class DashBoard extends React.Component {
     });
   };
 
-  getAirTable() {
-    const url = "https://api.airtable.com/v0/appQSPi3XUdUMbM1m/Projects";
+  getAirTable = async () => {
+    const url = `https://api.airtable.com/v0/appQSPi3XUdUMbM1m/Projects?api_key=${process.env.REACT_APP_AIRTABLE_KEY}`;
 
-    fetch(url, {
-      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-    })
-      .then(response => response.json())
-      .then(responseData => {
-        console.log("data from Airtable", responseData);
-        const projectData = responseData.records;
-        console.log("projectData ", projectData);
-        this.setState({ projectData: projectData });
+    try {
+      const response = await axios(url);
+      console.log("get response", response);
+      const projectData = response.data.records;
+      this.setState({
+        projectData
       });
-  }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
-    // const projectList = this.state.projectData.map(projectitem => {
-    //   console.log(projectitem.fields.Name);
-    // });
     return (
       <div className="view-container dashboard">
         <div className="dashboard-content">
